34017d46fcc06a28f53ce752eef16a6c7936f127
Muhammad Talha Khan <hdmoviez1234@gmail.com>
2021-12-07 07:09:21 +0500

    DOM

---
+++ app.js
@@ -0,0 +1,25 @@
+// console.log(document.childNodes[0]);   
+
+// console.log(document.childNodes[1]);
+
+// console.log(document.childNodes[1].childNodes[2].childNodes[3].childNodes[1]);
+
+// var a = document.getElementById('pappa');
+// console.log(a)
+
+// var a = document.getElementById('pappa');
+// console.log(a.nodeType)
+// console.log(a.nodeName)
+// console.log(a.childNodes[2])
+
+// var a = document.getElementById('pappa');
+// console.log(a.childNodes[0].nodeValue)
+
+// var a = document.getElementById('pappa');
+// console.log(a.childNodes[1].parentNode);
+
+// var a = document.getElementById('pappa');
+// console.log(a.childNodes);
+
+// var b = document.getElementById('sib');
+// console.log(b.childNodes[0])
